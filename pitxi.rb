cdee70b08e560f76894d57b49ebabc24b7e7ceda
ajlowndes <ajlowndes@gmail.com>
2016-08-20 13:31:05 +1000

    sha error...again

--- pitxi.rb
+++ pitxi.rb
@@ -3,7 +3,7 @@ class Pitxi < Formula
   homepage "https://github.com/ajlowndes/PiTXi"
   url "https://github.com/ajlowndes/PiTXi/archive/0.8.8.tar.gz"
   version "0.8.8"
-  sha256 "de81b6dc1d1e341bce740a0129c68b2c2bd7730ef65bc8995e421ca1b628a757"
+  sha256 "375e4de1280c114b9683ffd04813c90baa9232c68220f3bc22e17ed5a5ab9f1f"
 
 
   depends_on "csv-fix"
