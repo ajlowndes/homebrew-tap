5739846549c5e1dc61698d48d11afa7a3e6aa6da
ajlowndes <ajlowndes@gmail.com>
2016-08-20 13:24:23 +1000

    updated pitxi to 0.8.8

--- pitxi.rb
+++ pitxi.rb
@@ -1,9 +1,9 @@
 class Pitxi < Formula
   desc "Downloads transaction data from Paypal SFTP and formats it for Xero"
   homepage "https://github.com/ajlowndes/PiTXi"
-  url "https://github.com/ajlowndes/PiTXi/archive/0.8.7.tar.gz"
-  version "0.8.7"
-  sha256 "3324faf2a7566a512504e8dc85bbf447ab6c9c234671a74f10373e5d1c1ccb20"
+  url "https://github.com/ajlowndes/PiTXi/archive/0.8.8.tar.gz"
+  version "0.8.8"
+  sha256 "de81b6dc1d1e341bce740a0129c68b2c2bd7730ef65bc8995e421ca1b628a757"
 
   depends_on "csv-fix"
 
