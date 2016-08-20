2980dfb83d1b06f0a9ab58be0b247b9f3520e5fc
ajlowndes <ajlowndes@gmail.com>
2016-08-20 14:05:06 +1000

    add sha

--- pitxi.rb
+++ pitxi.rb
@@ -3,7 +3,7 @@ class Pitxi < Formula
   homepage "https://github.com/ajlowndes/PiTXi"
   url "https://github.com/ajlowndes/PiTXi/archive/0.8.9.tar.gz"
   version "0.8.9"
-  sha256 ""
+  sha256 "9407996c83ab1d095e79cf4597980e2914b6c050d7925d68aa95e5a5dc471dac"
 
 
   depends_on "csv-fix"
